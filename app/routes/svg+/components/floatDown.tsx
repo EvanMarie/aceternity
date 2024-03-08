a976bcd648959729828196623f6d6dbfb99e6e99
Evan Marie Carr <evanmariecarr@gmail.com>
2024-03-08 02:55:57 -0500

    circle animations on falling circles

--- app/routes/svg+/components/floatDown.tsx
+++ app/routes/svg+/components/floatDown.tsx
@@ -1,4 +1,4 @@
-import { motion, Variants } from "framer-motion";
+import { animate, motion, Variants } from "framer-motion";
 import Center from "~/components/buildingBlocks/center";
 import Flex from "~/components/buildingBlocks/flex";
 
@@ -8,6 +8,8 @@ interface Circle {
   duration: number;
   delay: number;
   key: number;
+  animationDuration: number;
+  pulsateScale: number;
 }
 
 interface FloatDownProps {
@@ -29,6 +31,17 @@ interface FloatDownProps {
   horizontalMax?: number;
   verticalMax?: number;
   durationMultiplier?: number;
+  shimmerColor?: string;
+  shimmerOpacity?: number;
+  maxCircleAnimationDuration?: number;
+  minCircleAnimationDuration?: number;
+  minGlowScale?: number;
+  maxGlowScale?: number;
+  minShimmerWidth?: number;
+  maxShimmerWidth?: number;
+  minPulsateScale?: number;
+  maxPulsateScale?: number;
+  circleAnimation?: "pulsate" | "glow" | "shimmer" | "none";
 }
 
 const generateRandomValues = (
@@ -57,15 +70,37 @@ export default function FloatDown({
   radiusMax = 20,
   radiusMin = 5,
   horizontalMax = 200,
-  verticalMax = 40,
+  verticalMax = 20,
   minDuration = 5,
   maxDuration = 23,
   durationMultiplier = 20,
+  shimmerColor = "red",
+  shimmerOpacity = 1,
+  maxCircleAnimationDuration = 5,
+  minCircleAnimationDuration = 0.5,
+  minGlowScale = 0.8,
+  maxGlowScale = 1.2,
+  minShimmerWidth = 1,
+  maxShimmerWidth = 5,
+  minPulsateScale = 0.4,
+  maxPulsateScale = 1.6,
+  circleAnimation = "none",
 }: FloatDownProps) {
   const numMovements =
     Math.floor(Math.random() * (maxMovements - minMovements + 1)) +
     minMovements;
 
+  const circleAnimationDuration =
+    Math.random() * (maxCircleAnimationDuration - minCircleAnimationDuration) +
+    minCircleAnimationDuration;
+  const pulsateScale =
+    circleAnimation === "pulsate"
+      ? Math.random() * (maxPulsateScale - minPulsateScale) + minPulsateScale
+      : 0;
+  const shimmerWidth =
+    circleAnimation === "shimmer"
+      ? Math.random() * (maxShimmerWidth - minShimmerWidth) + minShimmerWidth
+      : 0;
   const circleVariants: Variants = {
     initial: (custom: Circle) => ({
       cx: custom.x,
@@ -86,18 +121,56 @@ export default function FloatDown({
         filter: "drop-shadow(2vh 2vh 2vh rgba(0, 0, 0, 1))",
       },
     }),
+    shimmer: {
+      strokeOpacity: [shimmerOpacity, 0],
+      transition: {
+        duration: circleAnimationDuration,
+        repeat: Infinity,
+        repeatType: "reverse",
+      },
+    },
+    pulsate: (custom: Circle) => ({
+      scale: [1, custom.pulsateScale],
+      transition: {
+        duration: custom.animationDuration,
+        repeat: Infinity,
+        repeatType: "reverse",
+      },
+    }),
+    glow: (custom: Circle) => ({
+      scale: [minGlowScale, maxGlowScale],
+      transition: {
+        duration: custom.animationDuration,
+        repeat: Infinity,
+        repeatType: "mirror",
+        ease: "easeInOut",
+      },
+    }),
   };
 
-  const circles: Circle[] = Array.from({ length: numCircles }, (_, index) => ({
-    x: Math.random() * viewBoxWidth,
-    y: [
-      -60,
-      ...generateRandomValues(numMovements - 1, -verticalMax, verticalMax),
-    ],
-    duration: Math.random() * (maxDuration - minDuration) + minDuration,
-    delay: Math.random() * durationMultiplier,
-    key: index,
-  }));
+  const circles: Circle[] = Array.from({ length: numCircles }, (_, index) => {
+    const pulsateScale =
+      circleAnimation === "pulsate"
+        ? Math.random() * (maxPulsateScale - minPulsateScale) + minPulsateScale
+        : 0;
+    const animationDuration =
+      Math.random() *
+        (maxCircleAnimationDuration - minCircleAnimationDuration) +
+      minCircleAnimationDuration;
+
+    return {
+      x: Math.random() * viewBoxWidth,
+      y: [
+        -60,
+        ...generateRandomValues(numMovements - 1, -verticalMax, verticalMax),
+      ],
+      key: index,
+      duration: Math.random() * (maxDuration - minDuration) + minDuration,
+      delay: Math.random() * durationMultiplier,
+      animationDuration,
+      pulsateScale,
+    };
+  });
 
   return (
     <Flex className="shadowBroadNormal">
@@ -113,9 +186,20 @@ export default function FloatDown({
               <motion.circle
                 key={circle.key}
                 initial="initial"
-                animate="animate"
+                animate={[
+                  "animate",
+                  circleAnimation === "pulsate"
+                    ? "pulsate"
+                    : circleAnimation === "shimmer"
+                    ? "shimmer"
+                    : circleAnimation === "glow"
+                    ? "glow"
+                    : "",
+                ]}
                 variants={circleVariants}
                 fill={circleColor}
+                stroke={shimmerColor ? shimmerColor : circleColor}
+                strokeWidth={shimmerWidth ? shimmerWidth : 0.2}
                 custom={circle}
               />
             ))}
