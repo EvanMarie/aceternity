ef0d8b6b356ab2c8234561a0fc1af9d85faf4467
Evan Marie Carr <evanmariecarr@gmail.com>
2024-03-21 13:39:29 -0400

    image carousel animated

--- app/routes/design+/test.tsx
+++ app/routes/design+/test.tsx
@@ -44,27 +44,70 @@
 
 import React, { useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
-// import "./carousel.scss";
-import { useEffect } from "react";
-const Carousel = ({ Data }) => {
+import Button from "~/components/buildingBlocks/button";
+import CenterFull from "~/components/buildingBlocks/centerFull";
+const randomNumberGenerator = (min: number, max: number) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+const images = [
+  `https://picsum.photos/id/${randomNumberGenerator(1, 100)}/700/700`,
+  `https://picsum.photos/id/${randomNumberGenerator(1, 100)}/700/700`,
+  `https://picsum.photos/id/${randomNumberGenerator(1, 100)}/700/700`,
+  `https://picsum.photos/id/${randomNumberGenerator(1, 100)}/700/700`,
+  `https://picsum.photos/id/${randomNumberGenerator(1, 100)}/700/700`,
+  `https://picsum.photos/id/${randomNumberGenerator(1, 100)}/700/700`,
+  `https://picsum.photos/id/${randomNumberGenerator(1, 100)}/700/700`,
+  `https://picsum.photos/id/${randomNumberGenerator(1, 100)}/700/700`,
+  `https://picsum.photos/id/${randomNumberGenerator(1, 100)}/700/700`,
+  `https://picsum.photos/id/${randomNumberGenerator(1, 100)}/700/700`,
+];
+
+export function Carousel({
+  images,
+  imageSize = 50,
+  rounded = "rounded-[4vh]",
+  containerDimensions,
+  shadow = "metallicEdgesMd",
+  backImageBrightness = "brightness(25%)",
+  backImageXOffset = "27vh",
+  backImageYOffset = "8vh",
+  shiftDurationOffset = 0.75,
+}: {
+  images?: string[];
+  imageSize?: number;
+  rounded?: string;
+  backImageBrightness?: string;
+  shadow?: string;
+  containerDimensions?: string;
+  backImageXOffset?: string;
+  backImageYOffset?: string;
+  shiftDurationOffset?: number;
+}) {
+  if (!Array.isArray(images)) {
+    return null; // or some fallback UI
+  }
+  const imageDimensions = `w-[${imageSize}vh] h-[${imageSize}vh]`;
+
+  const imageContainerClassName = `absolute ${imageDimensions} ${rounded} bg-center bg-cover bg-no-repeat`;
+  const imageClassName = `w-full h-full ${rounded} ${shadow}`;
   const [FlowDirection, setFlowDirection] = useState(true);
-  const [CenterId, setCenterId] = useState(0);
-  const [LeftId, setLeftId] = useState(Data.length - 1);
+  const [FrontId, setFrontId] = useState(0);
+  const [LeftId, setLeftId] = useState(images.length - 1);
   const [RightId, setRightId] = useState(1);
 
   const nextBtn = () => {
-    if (LeftId === Data.length - 1) {
+    if (LeftId === images.length - 1) {
       setLeftId(0);
     } else {
       setLeftId(LeftId + 1);
     }
-    if (CenterId === Data.length - 1) {
-      setCenterId(0);
+    if (FrontId === images.length - 1) {
+      setFrontId(0);
     } else {
-      setCenterId(CenterId + 1);
+      setFrontId(FrontId + 1);
     }
 
-    if (RightId === Data.length - 1) {
+    if (RightId === images.length - 1) {
       setRightId(0);
     } else {
       setRightId(RightId + 1);
@@ -74,17 +117,17 @@ const Carousel = ({ Data }) => {
   const prevBtn = () => {
     setFlowDirection(false);
     if (LeftId === 0) {
-      setLeftId(Data.length - 1);
+      setLeftId(images.length - 1);
     } else {
       setLeftId(LeftId - 1);
     }
-    if (CenterId === 0) {
-      setCenterId(Data.length - 1);
+    if (FrontId === 0) {
+      setFrontId(images.length - 1);
     } else {
-      setCenterId(CenterId - 1);
+      setFrontId(FrontId - 1);
     }
     if (RightId === 0) {
-      setRightId(Data.length - 1);
+      setRightId(images.length - 1);
     } else {
       setRightId(RightId - 1);
     }
@@ -92,58 +135,65 @@ const Carousel = ({ Data }) => {
 
   const variants = {
     center: {
-      x: "0rem",
+      x: "0vh",
+      y: "0vh",
       opacity: 1,
-      scale: 1.1,
+      scale: 1.15,
       zIndex: "5",
       filter: "brightness(100%)",
-      backgroundImage: "url(" + Data[CenterId] + ")",
-      boxShadow: "0px 0px 30px 0px rgba(0,0,0,0.3)",
+      backgroundImage: "url(" + images[FrontId] + ")",
       transition: {
         type: "spring",
-        duration: 1,
+        duration: 1 * shiftDurationOffset,
+        filter: { type: "tween" },
       },
     },
     left: {
-      x: "-6rem",
+      x: "-" + backImageXOffset,
+      y: backImageYOffset,
       opacity: 1,
-      filter: "brightness(40%)",
+      filter: backImageBrightness,
       scale: 1,
-      backgroundImage: "url(" + Data[LeftId] + ")",
+      backgroundImage: "url(" + images[LeftId] + ")",
       zIndex: "4",
-      boxShadow: "unset",
+
       transition: {
         type: "spring",
-        duration: 1,
+        duration: 2 * shiftDurationOffset,
+        filter: { type: "tween" },
       },
     },
     right: {
-      backgroundImage: "url(" + Data[RightId] + ")",
-      x: "6rem",
+      backgroundImage: "url(" + images[RightId] + ")",
+      x: backImageXOffset,
+      y: backImageYOffset,
       opacity: 1,
-      filter: "brightness(40%)",
+      filter: backImageBrightness,
       scale: 1,
-      boxShadow: "unset",
+
       zIndex: "3",
       transition: {
         type: "spring",
-        duration: 1,
+        duration: 3 * shiftDurationOffset,
+        filter: { type: "tween" },
       },
     },
     rightHidden: {
-      x: "8rem",
+      x: "10rem",
       scale: 0,
       opacity: 0,
     },
     leftHidden: {
-      x: "-8rem",
+      x: "-10rem",
       scale: 0,
       opacity: 0,
     },
   };
   return (
-    <motion.div className="carousel-wrapper">
-      <motion.div className="carousel-content">
+    <motion.div
+      className={`flex flex-col gap-[10vh] items-center ${rounded} ${containerDimensions}`}
+    >
+      <motion.div className={`relative ${imageDimensions}`}>
         <AnimatePresence initial={false}>
           <motion.div
             key={LeftId}
@@ -151,57 +201,55 @@ const Carousel = ({ Data }) => {
             initial={FlowDirection ? "center" : "leftHidden"}
             animate="left"
             exit={"leftHidden"}
-            className="carousel-item"
-          ></motion.div>
+            className={`${imageContainerClassName}`}
+          >
+            <img
+              src={images[LeftId]}
+              alt="carousel item"
+              className={`${imageClassName}`}
+            />
+          </motion.div>
           <motion.div
             variants={variants}
-            key={CenterId}
+            key={FrontId}
             initial={FlowDirection ? "right" : "left"}
             animate="center"
-            className="carousel-item"
-          ></motion.div>
+            className={`${imageContainerClassName}`}
+          >
+            <img
+              src={images[FrontId]}
+              alt="carousel item"
+              className={`${imageClassName}`}
+            />
+          </motion.div>
           <motion.div
             key={RightId}
             variants={variants}
             initial={FlowDirection ? "rightHidden" : "center"}
             animate="right"
             exit={"rightHidden"}
-            className="carousel-item"
-          ></motion.div>
+            className={imageContainerClassName}
+          >
+            <img
+              src={images[RightId]}
+              alt="carousel item"
+              className={`${imageClassName}`}
+            />
+          </motion.div>
         </AnimatePresence>
       </motion.div>
-      <div className="carousel-btns">
-        <motion.button
-          initial={{ opacity: 0, scale: 0 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{
-            type: "spring",
-            duration: 0.5,
-          }}
-          whileHover={{ scale: 1.1 }}
-          whileTap={{ scale: 0.8 }}
-          className="bwd-btn"
-          onClick={prevBtn}
-        >
-          Back
-        </motion.button>
-        <motion.button
-          initial={{ opacity: 0, scale: 0 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{
-            type: "spring",
-            duration: 0.5,
-          }}
-          whileHover={{ scale: 1.1 }}
-          whileTap={{ scale: 0.8 }}
-          className="fwd-btn"
-          onClick={nextBtn}
-        >
-          Next
-        </motion.button>
+      <div className="mt-8 mb-8 flex justify-center space-x-4 z-10">
+        <Button onClick={prevBtn} buttonText="Previous" />
+        <Button onClick={nextBtn} buttonText="Next" />
       </div>
     </motion.div>
   );
-};
+}
 
-export default Carousel;
+export default function App() {
+  return (
+    <CenterFull>
+      <Carousel images={images} />
+    </CenterFull>
+  );
+}
